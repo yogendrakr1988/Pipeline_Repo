9a1f1dcb5b023d58d144325ce696a0f99df82ce0
Yogendra Kumar <yogendraidea@gmail.com>
2026-06-08 22:16:34 +0530

    For_Each 2 RGs & 2 STGs

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 resource "azurerm_resource_group" "rg" {
-    name = "rana-rg"
-    location = "Central India"
-  
+    for_each = var.rgs
+    name = each.value.name
+    location = each.value.location
+  }
+variable "rgs" {}
+
+resource "azurerm_storage_account" "stg" {
+    depends_on = [ azurerm_resource_group.rg ]
+    for_each = var.stg
+  name                     = each.value.name
+  resource_group_name      = each.value.resource_group_name
+  location                 = each.value.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
+
+variable "stg" {}
